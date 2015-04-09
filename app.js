fb57c0b52325e8d6cf6e70b7a6d9470a174bae8d
mihaineacsu <mihai.mneacsu@gmail.com>
2015-04-09 06:11:56 +0300

    In case of error return json with error message

--- app.js
+++ app.js
@@ -12,20 +12,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-  next();
+    res.setHeader('Content-Type', 'application/json');
+    next();
 });
 
 app.use('/', routes);
 
 app.use(function(err, req, res, next) {
     if(!err)
-    	return next();
-    
+        return next();
+
     console.log(err.stack);
     console.log(err);
 	res.status(500);
-	return res.send(err);
+	return res.send({
+        error: err.message
+    });
 });
 
 app.listen(config.get('PORT'));
